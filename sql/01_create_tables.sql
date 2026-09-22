1df3c8d10d0d41831cb8ce54735131be43ff7de9
Parshuram <ramparshu0137@gmail.com>
2026-09-22 19:36:37 +0530

    Update 01_create_tables.sql

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -4,7 +4,6 @@
 CREATE TABLE customers (
     customer_id VARCHAR(20),
     customer_name VARCHAR(100),
-    email VARCHAR(150),
     city VARCHAR(100),
     state VARCHAR(100),
     signup_date DATE
